6e8c8cb95c0b5017f13688642549e4080bc9eb8a
Emily Song <song.emily2004@gmail.com>
2023-11-19 01:06:53 -0800

    qweqwe

--- cpu.sv
+++ cpu.sv
@@ -5,11 +5,18 @@ module cpu(clk, reset, s, load, in, out, N, V, Z, w);
     output N, V, Z, w;
 
     reg [15:0] current_intruction;
-    nsel;
-    opcode;
-    op;
+    reg nsel;
+    reg [2:0] opcode;
+    reg [2:0] ALUop;
+    reg [1:0] op;
+    reg [15:0] sximm5;
+    reg [15:0] sximm8;
+    reg [1:0] shift;
+    reg 
 
     //INSTRUCTION REGISTER: The instruction currently being executed is stored in the 16 bit instruction register
+    //Input: clk, load, in
+    //Output: current_instruction
     always @(posedge clk) begin
         case (load)
             1'b0 : current_intruction = current_intruction; //nothing happens
@@ -19,8 +26,10 @@ module cpu(clk, reset, s, load, in, out, N, V, Z, w);
     end
 
     //INSTRUCTION DECODER: (Combinational) Extracts information from the current instruction that is used to control the datapath. (takes in nsel from FSM)
+    //Input: current_instruction, nsel
+    //Output: opcode, op/ALuop, shift, readnum, writenum, sximm5, sximm8
     always @(*) begin
-        //get opcode op, ALUop, shift
+        //get opcode op/ALUop, shift
 
         //with nsel, make a mux for Rn, Rd, Rm to readnum and writenum
 
@@ -28,8 +37,9 @@ module cpu(clk, reset, s, load, in, out, N, V, Z, w);
     end
 
     //FSM: Sets the inputs to the datapath based on stuff. (takes in opcode and op from decoder)
+    always @(posedge clk) begin
 
-
+    end
 
 
 endmodule: cpu
